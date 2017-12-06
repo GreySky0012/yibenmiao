0856c348df94f97b19f7a5eb3f980b66c3f00154
qiyue <sx-shihf@dtdream.com>
2017-12-06 18:55:08 +0800

    Add log

--- ybm/index/views.py
+++ ybm/index/views.py
@@ -2,11 +2,14 @@
 from __future__ import unicode_literals
 
 from django.http import HttpResponse
-
+import logging
 from django.shortcuts import render
 
 # Create your views here.
 
+logger = logging.getLogger("ybm")
+
 
 def index(request):
+    logger.info("request index")
     return HttpResponse(u'main page')
--- ybm/settings.py
+++ ybm/settings.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Django settings for ybm project.
 
@@ -23,6 +24,53 @@ SECRET_KEY = 'n3(fjs@^usw+)_37wrpnz3^=6)5m4n!2p$8wk13vihfj^%bpv_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,   # 是否禁用默认日志
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },  # 针对 DEBUG = True 的情况
+    },
+    'formatters': {
+        'standard': {
+            'format': '%(levelname)s %(asctime)s %(pathname)s %(filename)s %(module)s %(funcName)s %(lineno)d: %(message)s'
+        },  # 对日志信息进行格式化，每个字段对应了日志格式中的一个字段，更多字段参考官网文档，我认为这些字段比较合适，输出类似于下面的内容
+            # INFO 2016-09-03 16:25:20,067 /home/ubuntu/mysite/views.py views.py views get 29: some info...
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+            'formatter': 'standard'
+        },
+        'file_handler': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': BASE_DIR + 'test.log',
+            'formatter': 'standard'
+        },  # 用于文件输出
+        'console': {
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+    },
+    'loggers': {
+        'ybm': {
+            'handlers': ['file_handler', 'console'],
+            'level': 'DEBUG',
+            'propagate': True  # 是否继承父类的log信息
+        },  # handlers 来自于上面的 handlers 定义的内容
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    }
+}
 
 ALLOWED_HOSTS = []
 
