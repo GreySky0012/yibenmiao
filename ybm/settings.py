7f5e2bccb338c617607766aa236e52de8078fd32
qiyue <sx-shihf@dtdream.com>
2017-12-08 16:03:45 +0800

    add static setting

--- ybm/settings.py
+++ ybm/settings.py
@@ -184,3 +184,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'ybm', 'page', 'static'),
+]
+# 这个是默认设置，Django 默认会在 STATICFILES_DIRS中的文件夹 和 各app下的static文件夹中找文件
+# 注意有先后顺序，找到了就不再继续找了
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder"
+)
